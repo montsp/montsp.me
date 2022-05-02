08f6da96f2b2667f74bf211629eecc42b8ecb97c
montsp <konbuda4@icloud.com>
2022-05-02 06:14:56 +0200

    20220502

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router' 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     { title: 'ホーム', url: '/home', icon: 'home' },
     { title: '記事一覧', url: '/articlelist', icon: 'book'},
     { title: '記事募集', url: '/articlemail', icon: 'create'},
     { title: 'About US', url: '/aboutus', icon: 'information-circle'}
   ];
-  constructor() {}
+  constructor(public router: Router,) {}
+
+  ngOnInit() {
+    this.router.events
+      .subscribe(params => {
+        if (params instanceof NavigationEnd) {
+          gtag('config', 'UA-227577724-1', { page_path: params.url });
+        }
+      });
+  }
 }
+ 
+
+
+
